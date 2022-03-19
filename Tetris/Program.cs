9e31631eb498aea57fa0c9c906335bac023acf0a
CanisBolt <canisbolt@gmail.com>
2022-03-19 11:52:10 +0300

    Добавьте файлы проекта.

---
+++ Tetris/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+
+            int x1 = 2;
+            int y1 = 3;
+            char c1 = '*';
+
+            Draw(x1, y1, c1);
+
+            int x2 = 5;
+            int y2 = 4;
+            char c2 = '$';
+
+            Draw(x2, y2, c2);
+            Console.ReadKey();
+        }
+
+        static void Draw(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
+        }
+    }
+}
