539be884e09ce3632b7b9d8adf09a3c288cf7b29
CanisBolt <canisbolt@gmail.com>
2022-03-24 22:44:17 +0300

    Lesson 12

--- Tetris/Field.cs
+++ Tetris/Field.cs
@@ -8,8 +8,8 @@ namespace Tetris
 {
     static class Field
     {
-        private static int _width = 40;
-        private static int _height = 30;
+        private static int _width = 20;
+        private static int _height = 20;
         private static bool[][] _heap;
 
         public static int Width
@@ -46,6 +46,53 @@ namespace Tetris
             for(int i = 0; i < Height; i++)
             {
                 _heap[i] = new bool[Width];
+            }
+        }
+
+        public static void TryDeleteLines()
+        {
+            for(int j = 0; j < Height; j++)
+            {
+                int counter = 0;
+
+                for (int i = 0; i < Width; i++)
+                {
+                    if (_heap[j][i])
+                        counter++;
+                }
+                if (counter == Width)
+                {
+                    DeleteLines(j);
+                    Redraw();
+                }
+            }
+        }
+
+        private static void DeleteLines(int line)
+        {
+            for(int j = line; j >= 0; j--)
+            {
+                for(int i =0; i < Width; i++)
+                {
+                    if (j == 0)
+                        _heap[j][i] = false;
+                    else
+                        _heap[j][i] = _heap[j - 1][i];
+                }
+            }
+        }
+
+        private static void Redraw()
+        {
+            for (int j = 0; j < Height; j++)
+            {
+                for (int i = 0; i < Width; i++)
+                {
+                    if (_heap[j][i])
+                        Drawer.DrawPoint(i, j);
+                    else
+                        Drawer.HidePoint(i, j);
+                }
             }
         }
 
--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -15,7 +15,7 @@ namespace Tetris
             Console.SetWindowSize(Field.Width, Field.Height);
             Console.SetBufferSize(Field.Width, Field.Height);
 
-            generator = new FigureGenerator(20, 0, '*');
+            generator = new FigureGenerator(Field.Width / 2, 0, Drawer.DEFAULT_SYMBOL);
             Figure currentFigure = generator.GetNewFigure();
 
             while (true)
@@ -36,6 +36,7 @@ namespace Tetris
             if (result == Result.HEAP_STRIKE || result == Result.DOWN_BORDER_STRIKE)
             {
                 Field.AddFigure(currentFigure);
+                Field.TryDeleteLines();
                 currentFigure = generator.GetNewFigure();
                 return true;
             }
